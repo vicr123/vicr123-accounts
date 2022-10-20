c9b921003e36263f19ca25772bf43fe04163fd13
Victor Tran <vicr12345@gmail.com>
2022-10-20 20:02:40 +1100

    Ensure only new keys are provisioned

--- Commands/PreregisterCommand.cs
+++ Commands/PreregisterCommand.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json;
 using Fido2NetLib;
 using Fido2NetLib.Objects;
 
@@ -8,19 +9,24 @@ public static class PreregisterCommand
 {
     public static async Task Invoke(string rpName, string rpId, string username, string userId, string authenticatorAttachment)
     {
+        var json = await JsonDocument.ParseAsync(Console.OpenStandardInput());
+
+        var existingCreds =
+            json.RootElement.GetProperty("existingCreds").Deserialize<IList<string>>().Select(x => JsonSerializer.Deserialize<SecurityKey>(Convert.FromBase64String(x))).ToList();
         
         var fido2 = new Fido2(new Fido2Configuration
         {
             ServerDomain = rpId,
             ServerName = rpName
         });
+        
 
         var options = fido2.RequestNewCredential(new Fido2User
         {
             DisplayName = username,
             Id = Encoding.UTF8.GetBytes(userId),
             Name = username
-        }, new List<PublicKeyCredentialDescriptor>(), new AuthenticatorSelection
+        }, existingCreds.Select(x => new PublicKeyCredentialDescriptor(x.CredentialId)).ToList(), new AuthenticatorSelection
         {
             RequireResidentKey = true,
             UserVerification = UserVerificationRequirement.Preferred,
